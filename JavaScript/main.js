e9dc8f6febfa4cf03026bd682b364859a86b5808
茂木康汰 <Kouta@motegiyasushi-no-MacBook.local>
2016-03-23 22:15:27 +0900

    ローカルでチャットできるようにした

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,3 +1,7 @@
 jQuery(function(){
-    
-})
+    jQuery('form').submit(function(){
+        jQuery('#chat_area').prepend('<p>' + jQuery('input[name="text"]').val() + '</p');
+        document.getElementById('text').value = "";
+        return false;
+    });
+});
