04bb6f51f8d43d64836dde9ee745261a35f889ed
茂木康汰 <Kouta@motegiyasushi-no-MacBook.local>
2016-03-24 19:27:09 +0900

    ユーザー名を設定できるようにした

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,8 +1,22 @@
+var name;
 jQuery(function(){
-    // 送信処理
-    jQuery('form').submit(function(){
-        jQuery('#chat_area').prepend('<p>' + jQuery('input[name="text"]').val() + '</p');
-        document.getElementById('text').value = "";
+    // 初期設定
+    jQuery('#glayLayer').fadeIn('slow');
+    jQuery('#overLayer').slideDown('slow');
+    // 名前送信処理
+    jQuery('#name_form').submit(function(){
+        var name_text = document.getElementById('name_text');
+        name = name_text.value;
+        name_text = "";
+        jQuery('#overLayer, #glayLayer').fadeOut('slow');
+        // ユーザーエリアにユーザーの表示
+        jQuery('#user_area').append('<p>' + name + '</p>');
+        return false;
+    });
+    // チャット送信処理
+    jQuery('#chat_form').submit(function(){
+        jQuery('#chat_area').prepend('<p>' + name + '>' + jQuery('input[name="text"]').val() + '</p');
+        document.getElementById('chat_text').value = "";
         return false;
     });
     // user_areaの非表示
@@ -19,5 +33,5 @@ jQuery(function(){
         jQuery('#next_area').hide();
         jQuery('#user_area').css('width', '150px');
         jQuery('#user_area').show('slow');
-    });
+    });    
 });
