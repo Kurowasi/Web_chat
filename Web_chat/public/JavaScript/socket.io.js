712938634fe3ce02c328e83a99d4f30cc0a3d4a3
Kouta Motegi <motegikouta@yahoo.co.jp>
2016-03-25 02:10:54 +0900

    チャットに名前も追加させた

--- Web_chat/public/JavaScript/socket.io.js
+++ Web_chat/public/JavaScript/socket.io.js
@@ -9,8 +9,8 @@ var initSocket = function(){
         console.log('接続解除');
     });    
     // get chat text
-    s.on('S_To_C_chatText', function(text){
-        console.log(text);
-        jQuery('#chat_area').prepend('<p>' + text + '</p>');
+    s.on('S_To_C_chatText', function(obj){
+        jQuery('#chat_area').prepend('<dd>' + obj.text + '</dd>');
+        jQuery('#chat_area').prepend('<dt>' + obj.name + '</dt>');
     });
 }
