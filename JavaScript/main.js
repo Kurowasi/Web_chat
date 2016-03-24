2e1399ab8a74fcc9ea5d2b251509c96f3c2c6d8a
茂木康汰 <Kouta@motegiyasushi-no-MacBook.local>
2016-03-24 17:55:56 +0900

    add User_area show and hidden

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,7 +1,23 @@
 jQuery(function(){
+    // 送信処理
     jQuery('form').submit(function(){
         jQuery('#chat_area').prepend('<p>' + jQuery('input[name="text"]').val() + '</p');
         document.getElementById('text').value = "";
         return false;
+    });
+    // user_areaの非表示
+    jQuery('#user_area a').click(function(){
+        jQuery('#user_area').animate({
+            'width': '0px'
+        }, 'slow', 'swing', function(){
+            jQuery('#user_area').hide('fast');
+            jQuery('#next_area').show('slow');
+        });
+    });
+    // user_areaの表示
+    jQuery('#next_area a').click(function(){
+        jQuery('#next_area').hide();
+        jQuery('#user_area').css('width', '150px');
+        jQuery('#user_area').show('slow');
     });
 });
